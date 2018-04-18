accb8418f04ff1670024ce061b35403e994c1acd
DESKTOP-KP5Q71B\danie <danielcelta2000@yahoo.com.br>
2018-04-18 20:05:58 -0300

    Initiated Pieces Placement
    GameManager object managing pieces and spawning them on the board.

---
+++ Super Tic Tac Toe/Assets/Scripts/Hit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour {
+
+	public GameObject Object;
+	private MatchManager _script; 
+
+	void Awake()
+	{
+		_script = Object.GetComponent<MatchManager>();
+	}
+
+	void OnMouseDown()
+	{
+		_script.SpawnNewPiece(this.gameObject);
+	}
+
+}
---
+++ Super Tic Tac Toe/Assets/Scripts/MatchManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour {
+
+	public GameObject Cross;
+	public GameObject Circle;
+	public enum Piece { Empty, Cross, Circle};
+	public Piece Turn;
+
+	public void SpawnNewPiece(GameObject obj)
+	{
+		if (Turn == Piece.Cross)
+		{
+			Instantiate(Cross, obj.transform.position, Quaternion.identity);
+			Turn = Piece.Circle;
+		}
+		else
+		{
+			Instantiate(Circle, obj.transform.position, Quaternion.identity);
+			Turn = Piece.Cross;
+		}
+
+		Destroy(obj.gameObject);
+	}
+}
